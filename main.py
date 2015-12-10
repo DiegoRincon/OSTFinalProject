af1530b8e32e3dbac5f8736d2206e9227969a9d3
Diego Rincon <drs414@nyu.edu>
2015-12-10 00:02:10 -0500

    Final tutorial commit

--- main.py
+++ main.py
@@ -1,17 +1,102 @@
+
+import os
+import cgi
+import urllib
+
 from google.appengine.api import users
+from google.appengine.ext import ndb
+
+import jinja2
 import webapp2
 
+JINJA_ENVT = jinja2.Environment(
+      loader=jinja2.FileSystemLoader(os.path.dirname(__file__)), extensions=['jinja2.ext.autoescape'], autoescape=True)
+
+MAIN_PAGE_FOOTER_TEMPLATE = """\
+      <form action="/sign?%s" method="post">
+	 <div><textarea name="content" rows="3" cols="60"></textarea></div>
+	 <div><input type="submit" value="Sign GuestBook"></div>
+      </form>
+      <hr>
+      <form>Guestbook name:
+	 <input value="%s" name="guestbook_name">
+	 <input type="submit" value="switch">
+      </form>
+      <a href="%s">%s</a>
+   </body>
+</html>
+"""
+      
+
+DEFAULT_GUESTBOOK_NAME = 'default_gestbook'
+
+def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
+   """ Constructs a Datastore key for a Guestbook entity.
+
+   We use guestbook_name as the ke
+   """
+   return ndb.Key('Gestbook', guestbook_name)
+
+class Author(ndb.Model):
+   """Sub model for representing an author."""
+
+   identity = ndb.StringProperty(indexed=False)
+   email = ndb.StringProperty(indexed=False)
+
+class Greeting(ndb.Model):
+   """A main model for representing an individual Guestbook entry."""
+
+   author = ndb.StructuredProperty(Author)
+   content = ndb.StringProperty(indexed=False)
+   date = ndb.DateTimeProperty(auto_now_add=True)
+
 class MainPage(webapp2.RequestHandler):
+
    def get(self):
-      #  Checks for active Google account session
+      guestbook_name = self.request.get('guestbook_name', DEFAULT_GUESTBOOK_NAME)
+      
+      greetings_query = Greeting.query(ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
+      greetings = greetings_query.fetch(10)
+
       user = users.get_current_user()
 
       if user:
-	 self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
-	 self.response.write('Hello, ' + user.nickname())
+	 url = users.create_logout_url(self.request.uri)
+	 url_linktext = 'logout'
       else:
-	 self.redirect(users.create_login_url(self.request.uri))
+	 url = users.create_login_url(self.request.uri)
+	 url_linktext = 'Login'
+
+      template_values = {
+	    'user': user,
+	    'greetings': greetings,
+	    'guestbook_name': urllib.quote_plus(guestbook_name),
+	    'url': url,
+	    'url_linktext': url_linktext,
+      }
+
+      template = JINJA_ENVT.get_template('index.html')
+      self.response.write(template.render(template_values))
+
+class Guestbook(webapp2.RequestHandler):
+   def post(self):
+      # We set the same parent key on the 'Greeting' to ensure each
+      # Greeting is in the same entity group.
+
+      guestbook_name = self.request.get('guestbook_name', DEFAULT_GUESTBOOK_NAME)
+      greeting = Greeting(parent=guestbook_key(guestbook_name))
+
+      if users.get_current_user():
+	 greeting.author = Author(identity=users.get_current_user().user_id(), email=users.get_current_user().email())
+
+      greeting.content = self.request.get('content')
+      greeting.put()
+
+      query_params = {'guestbook_name': guestbook_name}
+      self.redirect('/?' + urllib.urlencode(query_params))
+
 
 app = webapp2.WSGIApplication([
    ('/', MainPage),
+   ('/sign', Guestbook),
 ], debug=True)
