6f150301b162960eeb3930a14aecdf6b8cdd4020
Diego Rincon <drs414@nyu.edu>
2015-12-13 20:51:03 -0500

    Reservations are now validated against start and end time of resource, as well as against other reservations

--- main.py
+++ main.py
@@ -30,23 +30,28 @@ class Author(ndb.Model):
    identity = ndb.StringProperty(indexed=False)
    email = ndb.StringProperty(indexed=True)
 
-class Reservation(ndb.Model):
-   owner = ndb.StructuredProperty(Author)
-   startTime = ndb.DateTimeProperty(indexed=True)
-   duration = ndb.IntegerProperty(indexed=False)
-   resource = ndb.StringProperty(indexed=True)
-   reservationId = ndb.StringProperty(indexed=True)
+class Interval(ndb.Model):
+   startTime = ndb.TimeProperty(indexed=False)
+   endTime = ndb.TimeProperty(indexed=False)
 
 class Resource(ndb.Model):
    owner = ndb.StructuredProperty(Author)
    name = ndb.StringProperty(indexed=False)
+   availabilityIntervals = ndb.StructuredProperty(Interval,repeated=True)
    startTime = ndb.DateTimeProperty(indexed=True)
    endTime = ndb.DateTimeProperty(indexed=True)
    tags = ndb.StringProperty(repeated=True)
-   reservations = ndb.StructuredProperty(Reservation, repeated=True)
+   reservations = ndb.StringProperty(repeated=True)
    timeCreated = ndb.DateTimeProperty(auto_now_add=True)
    resourceId = ndb.StringProperty(indexed=True)
 
+class Reservation(ndb.Model):
+   owner = ndb.StructuredProperty(Author)
+   startDate = ndb.DateTimeProperty(indexed=True)
+   startTime = ndb.TimeProperty(indexed=True)
+   duration = ndb.IntegerProperty(indexed=False)
+   resource = ndb.StructuredProperty(Resource,indexed=True)
+   reservationId = ndb.StringProperty(indexed=True)
 
 class MainPage(webapp2.RequestHandler):
    def get(self):
@@ -56,18 +61,16 @@ class MainPage(webapp2.RequestHandler):
       resources = resource_query.fetch(1000)
 
       now = datetime.datetime.now()
-      allResources_query = Resource.query(
-	    ndb.query.AND(Resource.startTime >= now)).order(-Resource.startTime)
+      allResources_query = Resource.query(Resource.startTime >= now).order(-Resource.startTime)
       allResources = allResources_query.fetch(1000)
-
       allResources[:] = [res for res in allResources if res.owner.email != myEmail]
 
-      for resource in allResources:
-	 if (resource.owner.email == myEmail):
-	    allResources.remove(resource)
-
+      reservations_query = Reservation.query(Reservation.startDate >= now).order(Reservation.startDate)
+      reservations = reservations_query.fetch(1000)
+      reservations[:] = [res for res in reservations if res.owner.email ==myEmail]
 
       user = users.get_current_user()
+      nickname = str(user).split("@")[0]
 
       if user:
 	 url = users.create_logout_url(self.request.uri)
@@ -77,8 +80,9 @@ class MainPage(webapp2.RequestHandler):
 	 url_linktext = 'Login'
 
       template_values = {
-	    'user': user,
+	    'user': nickname,
 	    'resources': resources,
+	    'reservations' : reservations,
 	    'allResources': allResources,
 	    'url': url,
 	    'url_linktext': url_linktext,
@@ -94,8 +98,12 @@ class ResId(webapp2.RequestHandler):
       resource_query = Resource.query(Resource.resourceId == resId)
       resource = resource_query.fetch(1)[0]
 
+      reservation_query = Reservation.query(Reservation.resource.resourceId == resId)
+      reservations = reservation_query.fetch(100)
+
       template_values = {
 	    'resource' : resource,
+	    'reservations' : reservations,
 	    }
 
       template = JINJA_ENVT.get_template('resource.html')
@@ -115,14 +123,34 @@ class ReservationId(webapp2.RequestHandler):
       template = JINJA_ENVT.get_template('reservationId.html')
       self.response.write(template.render(template_values))
 
+def isTimeInInterval(timeStart, timeEnd, start, end):
+   return (timeStart >= start and timeEnd <= end) or (timeStart < start and timeEnd > start) or (timeStart < end and timeEnd > end)
+
 class MakeReservation(webapp2.RequestHandler):
    def get(self):
       resId = self.request.get('resId')
+      error = self.request.get('error')
       time.sleep(2)
       resource_query = Resource.query(Resource.resourceId == resId)
       resource = resource_query.fetch(1)[0]
+      
+      reservation_query = Reservation.query(Reservation.resource.resourceId == resId)
+      reservations = reservation_query.fetch(100)
+
+      if (error != ""):
+	 template_values = {
+	    'resource' : resource,
+	    'reservations' : reservations,
+	    'date' : resource.startTime.date().strftime("%d/%m/%Y"),
+	    'error' : "Reservation must be made within the availability period"
+	 }
+	 template = JINJA_ENVT.get_template('makeReservation.html')
+	 self.response.write(template.render(template_values))
+	 return
+      
       template_values = {
 	    'resource' : resource,
+	    'reservations' : reservations,
 	    'date' : resource.startTime.date().strftime("%d/%m/%Y")
 	    }
       template = JINJA_ENVT.get_template('makeReservation.html')
@@ -136,20 +164,52 @@ class MakeReservation(webapp2.RequestHandler):
 
       author = Author(identity=users.get_current_user().user_id(), email=users.get_current_user().email())
       startTime = datetime.datetime.strptime(dateString + " " + startTimeString, DATETIME_FORMAT)
+
       resource_query = Resource.query(Resource.resourceId == resId)
       resource = resource_query.fetch(1)[0]
+
+      endTime = startTime + datetime.timedelta(minutes=int(duration))
+
+      start = startTime.time()
+      end = endTime.time()
+
+      """
+      Check that the reservation is within the start and end, and that it doesn't conflict
+      with other reservations
+      """
+      
+      if (isTimeInInterval(startTime, endTime, resource.startTime, resource.endTime) == False):
+	 query_params = { 'error' : 1, 'resId' : resId }
+	 self.redirect('/reservation?' + urllib.urlencode(query_params))
+	 return
+
+
+      if resource.availabilityIntervals:
+	 for interval in resource.availabilityIntervals:
+	    logging.info(str(interval))
+	    if (isTimeInInterval(start,end,interval.startTime,interval.endTime)):
+	       query_params = { 'error' : 1, 'resId' : resId }
+	       self.redirect('/reservation?' + urllib.urlencode(query_params))
+	       return
 
       reservationId = str(uuid.uuid1())
       
       reservation = Reservation(parent=reservation_key(reservationId),
 	    owner=author,
-	    startTime=startTime,
-	    resource=resource.resourceId,
+	    startTime=startTime.time(),
+	    startDate=startTime,
+	    resource=resource,
 	    duration=int(duration),
 	    reservationId=reservationId,
 	    )
       reservation.put()
-      resource.reservations.append(reservation)
+      resource.reservations.append(reservation.reservationId)
+      if resource.availabilityIntervals:
+	 resource.availabilityIntervals.append(Interval(startTime=start, endTime=end))
+      else:
+	 resource.availabilityIntervals = [Interval(startTime=start,endTime=end)]
+
+      resource.put()
 
       query_params = { 'resId' : reservationId }
       self.redirect('/reservationId?' + urllib.urlencode(query_params))
@@ -168,6 +228,9 @@ class MakeResource(webapp2.RequestHandler):
 
       author = Author(identity=users.get_current_user().user_id(), email=users.get_current_user().email())
 
+      """
+      availabilityIntervals = list(Interval(startTime.time(), endTime.time())
+      """
        
       resource = Resource(parent=resource_key(resourceId),
 	    owner=author,
